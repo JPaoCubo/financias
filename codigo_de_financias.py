d5577db1f48cbd93f29d557412d672cf0d911e93
JPaoCubo <joaopedroaz2006@gmail.com>
2026-05-18 22:31:19 -0300

    Add files via upload

---
+++ codigo_de_financias.py
@@ -0,0 +1,12 @@
+#agora irei testar sobre como criar, e adicionar itens a um dicionario
+#primeiro irei testar como funciona as variaveis dentro do dicionario
+
+categoria=input("digite a categoria: ")
+valor=input("digite o valor: ")
+
+despesas={
+    "categoria":[categoria],
+    "valor":[valor]
+}
+
+print(despesas)
